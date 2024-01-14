469271136c182c356b078028b26c74aa6d078d6c
Dayne Lalmond <dlalmondo@gmail.com>
2024-01-13 20:44:50 -0500

    connect page layout

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -7,25 +7,23 @@ function Connect() {
       <h3>Let's connect</h3>
       
       <section className="topics">
-        <div id="stickyscroll">
-        <mark>Study Server</mark>
-        <h4>Sticky scroll</h4>
-        <p className="blogs"><b>T</b>he tools you choose are important. Visual Studio Code is the
-          platform in which I learned to code. Not only does it provide an abundance of useful settings,
-          everything is customizable! The topic of this matter is sticky scroll. Like sticky position
-          for CSS, sticky scroll positions your scope to remain at the top as you scroll. It is a method
-          to keep track of functions or classes that stretch beyond your vertical window. To enable, go to
-          your settings ⇨ text editor ⇨ enable sticky scroll. You can easily toggle this setting from the
-          view option.</p>
-          </div>
-        <mark>Contact Me</mark>
-        <h4>Save bookmarks</h4>
-        <p className="blogs"><b>I</b>f you're like me, stuying code may often bring about several tabs.
-          Perhaps, you want to use pieces here and there. However, with more open tabs, the false sense
-          of quickly finding answers could become more desirable. While it may be a simple task, categorized
-          bookmarks could help reduce the flaws of solution scanning and allow you to better review any
-          new material. This not only declutters what's in front of you but promotes the focus of reading
-          to understand.</p>
+        {/* <mark>Contact Me</mark> */}
+        <h4>Fill the form</h4>
+        <p className="blogs"><b>Y</b>es, it really works. By submitting this completed form, it will notify me.</p>
+        <div>
+          <form>
+          <input type="text" placeholder="Name"/>
+          <input type="text" placeholder="Message"/>
+          <input type="button" value="Send"/>
+          </form>
+
+        </div>
+        <h4>Study with me</h4>
+        <p className="blogs"><b>J</b>oin my study server. Enter your discord username and send a request to join.</p>
+        <form>
+          <input type="text" placeholder="Username"/>
+          <input type="button" value="Request"/>
+          </form>
       </section>
     </main>
   );
