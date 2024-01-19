55f9f56ac348e6bd896ae998479b1d9e0a2a966b
Dayne Lalmond <dlalmondo@gmail.com>
2024-01-19 10:12:02 -0500

    finished connect section layout

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -3,27 +3,31 @@ import '../styles/index.css';
 function Connect() {
 
   return (
+
     <main>
       <h3>Let's connect</h3>
-      
+
       <section className="topics">
         {/* <mark>Contact Me</mark> */}
-        <h4>Fill the form</h4>
-        <p className="blogs"><b>Y</b>es, it really works. By submitting this completed form, it will notify me.</p>
+        <h4 className="about-me"> Fill the form</h4>
+        <p className="first-p"><b>Y</b>es, it really works. By submitting this completed form, it will notify me via email. However, I prefer connecting 
+        through <strong><a href="https://www.linkedin.com/in/dayne-lalmond/" target="_blank">LinkedIn</a></strong>. I'm using <strong><a href="https://www.emailjs.com/" target="_blank">EmailJS</a></strong> to enable this form. </p>
         <div>
-          <form>
-          <input type="text" placeholder="Name"/>
-          <input type="text" placeholder="Message"/>
-          <input type="button" value="Send"/>
+          <form id="contact-me">
+            <input type="text" placeholder="Name" />
+            <textarea type="text" rows="4" maxlength="124" />
+            <input type="button" value="Send" />
+
           </form>
 
         </div>
-        <h4>Study with me</h4>
-        <p className="blogs"><b>J</b>oin my study server. Enter your discord username and send a request to join.</p>
-        <form>
-          <input type="text" placeholder="Username"/>
-          <input type="button" value="Request"/>
-          </form>
+        <h4 className="study">Study with me</h4>
+        <p className="second-p"><b>J</b>oin me in web development research. Enter your slack username to request joining my slack study group <b>"SSG"</b>. Your 
+        username is captured via webhook and sent to the slack server. Once accepted, remember to introduce yourself.</p>
+        <form className="study" id="study-group">
+          <input type="text" placeholder="Username" />
+          <input type="button" value="Study" />
+        </form>
       </section>
     </main>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,14 +26,14 @@ function Home() {
         <img src={avatar} id="profile" alt="Avatar" />
         <h3>Hello, I'm Dayne</h3>
         <section className="topics">
-          <h4 id="about-me">About Me</h4>
+          <h4 className="about-me">About Me</h4>
           <p className="first-p"><b>J</b>unior full-stack web developer, aspiring software engineer, and ambitious innovator. Focused primarily
             on <strong className="bold">MERN</strong> stack applications, I study the tools and material to expand my skillset. Additionally, I love 
             creating automation. If you're interested in some of my completed automation, see my <strong><a href="#featured">featured</a></strong> project. 
             Be sure to check out the Discord server provided. On my personal time,
             I enjoy reading, exercising, and listening to music.</p>
             
-          <h4 id="dev-blog">My Studies</h4>
+          <h4 className="study">My Studies</h4>
           <p className="second-p"><b>S</b>tart by understanding. With logical reasoning, complex breakthroughs, and additional tips, coding becomes simpler. 
             The best sources to learn from come with good <strong className="bold">documentation</strong>. While developing a range of new material, remembering 
             to review and save comments completes your work. See my <strong><NavLink to="/projects">
@@ -48,7 +48,7 @@ function Home() {
         {inView ? (
           <div className={isVisible ? "isVisible" : ""}>
             <div className="featured-div">
-              <img src={holiday} alt="logo" id="holiday" />
+            <a rel="noreferrer" href="https://www.theholiday.net/" target="_blank" className="ext-link"><img src={holiday} alt="logo" id="holiday" /></a>
               <p className="title">{title[0]}</p>
               <p className="description"><b>Q</b>uality multiplatform Minecraft community. Check out the <strong><a rel="noreferrer" href="https://www.theholiday.net/" target="_blank" className="ext-link">Website</a></strong>. Discuss on <strong><a rel="noreferrer" href="https://discord.gg/jzvyY2tFnQ" target="_blank" className="ext-link">Discord</a></strong>. Support me on <strong><a rel="noreferrer" href="https://www.patreon.com/holidaye" target="_blank" className="ext-link">Patreon</a></strong>.</p>
 
